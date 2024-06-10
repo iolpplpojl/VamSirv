1885e46e64319c3aa1406e924924bb88b12718d3
didwlwd <didehdals321@naver.com>
2024-06-11 00:31:32 +0900

    20240611_1

--- Sirv/Assets/Scripts/System/Option.cs
+++ Sirv/Assets/Scripts/System/Option.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Option : MonoBehaviour
 {
+    FullScreenMode screenMode;
+    public Dropdown resolutionDropdown;
+    public Toggle fullscreenBtn;
+    List<Resolution> resolutions = new List<Resolution>();
+    public int resolutionNum;
+
     public Slider sfxSound;
     public Slider songSound;
     public Image dmgBtn;
     public bool showDamage;
 
     // Start is called before the first frame update
+
+    [System.Obsolete]
     void Start()
     {
         sfxSound.value = SaveSystem.instance.data.sfxSound;
@@ -23,9 +31,10 @@ public class Option : MonoBehaviour
         {
             dmgBtn.color = Color.white;
         }
+        InitUI();
+        
     }
 
-    // Update is called once per frame
     public void sfxSoundChange()
     {
         SaveSystem.instance.setAudio("SFX",sfxSound.value);
@@ -47,5 +56,48 @@ public class Option : MonoBehaviour
         }
         SaveSystem.instance.data.showDmg = showDamage;
 
+    }
+
+    [System.Obsolete]
+    void InitUI()
+    {
+        for(int i = 0; i<Screen.resolutions.Length; i++)
+        {
+            if (Screen.resolutions[i].refreshRate == 60)
+                resolutions.Add(Screen.resolutions[i]);
+        }
+        resolutions.AddRange(Screen.resolutions);
+        resolutionDropdown.options.Clear();
+
+        int optionNum = 0;
+
+        foreach (Resolution item in resolutions)
+        {
+            Dropdown.OptionData option = new Dropdown.OptionData();
+            option.text = item.width + " x " + item.height;
+            resolutionDropdown.options.Add(option);
+
+            if (item.width == Screen.width && item.height == Screen.height)
+                resolutionDropdown.value = optionNum;
+            optionNum++;
+        }
+        resolutionDropdown.RefreshShownValue();
+
+        fullscreenBtn.isOn = Screen.fullScreenMode.Equals(FullScreenMode.FullScreenWindow) ? true : false;
+    }
+    public void DropboxOptionChange(int x)
+    {
+        resolutionNum = x;
+    }
+
+    public void FullScreenBtn(bool isFull)
+    {
+        screenMode = isFull ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed;
+    }
+    
+    public void OkBtnClick()
+    {
+        Screen.SetResolution(resolutions[resolutionNum].width,
+            resolutions[resolutionNum].height,screenMode);
     }
 }
